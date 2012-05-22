531dd8428a52d0d688c44b476c8c1ebde9afc946
Mikel Larreategi <mlarreategi@codesyntax.com>
2012-05-22 16:22:26 +0200

    remove unneeded code

--- cs/rtmpstreaming/__init__.py
+++ cs/rtmpstreaming/__init__.py
@@ -3,6 +3,3 @@ from zope.i18nmessageid import MessageFactory
 
 rtmpstreamingMessageFactory = MessageFactory('cs.rtmpstreaming')
 
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
